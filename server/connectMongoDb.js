258e89d2d958797c1b0e69373097fd50390a236a
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2023-12-17 18:43:35 +0530

    db changes

--- server/connectMongoDb.js
+++ server/connectMongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL || "mongodb://127.0.0.1:27017/StackOverFlow", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
